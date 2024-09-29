9460629063debe146eda2fa3f02a8fc0e9bbc844
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-09-29 20:39:34 +0200

    Refactor website URLs and views

--- website/urls.py
+++ website/urls.py
@@ -8,16 +8,15 @@ from website import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('index', views.home, name='home'),
     path('home', views.home, name='home'),
 
     path('aboutme', views.aboutme, name='aboutme'),
 
-    path('post', views.posts, name='posts'),
-    path('post/', views.posts, name='posts'),
+    path('post', views.posts, name='post'),
+    path('post/', views.posts, name='post'),
     path('post/<slug>/', views.post, name = 'post'),
 
-    path('category', views.categories, name='categories'),
+    path('category', views.categories, name='category'),
     path('category/', views.categories, name='categories'),
     path('category/<slug>/', views.category, name = 'category'),
     
